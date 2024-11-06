73642c711e4f50d023823a6877b8a960ee31d9d3
Liu Yuhe <171144077+liuyuhe666@users.noreply.github.com>
2024-11-06 21:06:10 +0800

    fix: update

--- cmd/root.go
+++ cmd/root.go
@@ -11,7 +11,7 @@ import (
 const secretKey = "XOR_SECRET"
 
 var rootCmd = &cobra.Command{
-	Use:   "xor <filename>",
+	Use:   "go-file-xor <filename>",
 	Short: "go file xor",
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) != 1 {
